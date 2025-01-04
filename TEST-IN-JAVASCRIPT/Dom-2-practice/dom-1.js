8440e42fcba5ad03ec1cdd7ffb0e382bfb605450
anilkumar <9012kumaranil@gmail.com>
2025-01-03 18:58:45 -0800

    first commit

--- TEST-IN-JAVASCRIPT/Dom-2-practice/dom-1.js
+++ TEST-IN-JAVASCRIPT/Dom-2-practice/dom-1.js
@@ -26,7 +26,51 @@
             //    offsetTop & offsetLeft 
 
 
-var box = document.querySelector("#box");
+// var box = document.querySelector("#box");
 
-console.log("Box offset Top : " + box.offsetTop);
-console.log("Box offset Left : " + box.offsetLeft);
+// console.log("Box offset Top : " + box.offsetTop);
+// console.log("Box offset Left : " + box.offsetLeft);
+
+            //   scrollTop & scrollLeft
+
+// let targetbox = document.querySelector("#box-two");
+
+// targetbox.addEventListener("scroll", () => {
+//     console.clear();
+//     console.log("scroll Top : " + targetbox.scrollTop);
+//     console.log("scroll Left : " + targetbox.scrollLeft);
+// });
+
+
+                //  scrollWidth & scrollHeight
+
+// var targetbox_three = document.querySelector("#box-three");
+
+// var scroll_width = targetbox_three.scrollWidth;
+// var scroll_height = targetbox_three.scrollHeight;
+
+// console.log("Scroll Widht : " + scroll_width);
+// console.log("Scroll Height : " + scroll_height);
+
+                //  offsetWidth & offsetHeight
+
+// var target_box_four = document.querySelector("#box-four");
+
+// var offsetwidth = target_box_four.offsetWidth;
+// var offsetheight = target_box_four.offsetHeight;
+
+// console.log("offsetwidth : " + offsetwidth);
+// console.log("offsetheight : " + offsetheight);  
+
+
+                //    clientWidth & clientHeight
+
+
+var targetclient = document.querySelector("#box-5");
+
+var clientwidth = targetclient.clientWidth;
+var clientheight = targetclient.clientHeight;
+
+console.log("ClientWidth : " + clientwidth);
+console.log("ClientHeight : " + clientheight);
+                
---
+++ TEST-IN-JAVASCRIPT/Dom-2-practice/dom-2.js
@@ -0,0 +1,64 @@
+        //    ClientX & ClientY
+
+// document.addEventListener("mousehover", function(element){
+//     var x = element.clientX;
+//     var y = element.clientY;
+
+//     console.clear();
+
+//     console.log("ClientX value : " + x);
+//     console.log("ClientY value : " + y);
+// })  
+
+// doosra tarika box-1 per clienX & client Y 
+
+// var target1 = document.querySelector("#box-1");
+
+// target1.addEventListener("click", function(element){
+//     console.clear();
+
+//     var x = element.clientX;
+//     var y = element.clientY;
+
+//     console.log("ClientX value : " + x);
+//     console.log("ClientY value : " + y);
+// })
+
+
+                //   pageX & pageY
+
+
+// document.addEventListener("mousemove", function(element){
+//     var x = element.pageX;
+//     var y = element.pageY;
+
+//     var x2 = element.clientX;
+//     var y2 = element.clientY;
+
+//     console.clear();
+
+//     console.log("pageX value : " + x);
+//     console.log("ClientX2 value : " + x2);
+
+//     console.log("pageY value : " + y);
+//     console.log("ClientY value : " + y2);
+// })  
+
+                //   screenX & screenY
+
+
+document.querySelector("#box-2").addEventListener("click", function(elvent){
+    var x = elvent.screenX;
+    var y = elvent.screenY;
+
+    var x2 = elvent.clientX;
+    var y2 = elvent.clientY;
+
+    console.clear();
+
+    console.log("screenX value : " + x);
+    console.log("ClientX2 value : " + x2);
+
+    console.log("screenY value : " + y);
+    console.log("ClientY value : " + y2);
+})  
