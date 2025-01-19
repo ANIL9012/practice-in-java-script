ef2332b69c02558d91086fdfc3c300872215e38b
anilkumar <9012kumaranil@gmail.com>
2025-01-18 20:42:52 -0800

    first commit

--- practice-assignment/practice-assignment.js
+++ practice-assignment/practice-assignment.js
@@ -321,5 +321,21 @@
         
         //    Execution Context in JavaScript
 
-
         
+
+        // outer[(bahar bale function ko kahte he Heigher function),(Inner function ko kahte he callback function)]
+
+        function outer() {
+            const a = 4
+            function parent() {
+              const b = 6
+              return function() {
+                console.log(a + b)
+              }
+            }
+            return parent()
+          }
+          
+          const add1 = outer()
+          
+          console.dir(add1)
