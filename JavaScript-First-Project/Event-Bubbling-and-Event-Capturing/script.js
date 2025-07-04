727c29c9d63c578ff1a14e92959e4c350d67c341
anil <9012kumaranil@gmail.com>
2025-07-04 11:21:21 +0530

    3 goals project

---
+++ JavaScript-First-Project/Event-Bubbling-and-Event-Capturing/script.js
@@ -0,0 +1,63 @@
+
+
+const green = document.querySelector(".green");
+const pink = document.querySelector(".pink");
+const blue = document.querySelector(".blue")
+
+
+// green.addEventListener('click', (e) => {
+//     console.log("Green Event Listners");
+// });
+
+
+// pink.addEventListener('click', (e) => {
+//     console.log("Pink Event Listners");
+// });
+
+// blue.addEventListener('click', (e) => {
+//     console.log("Blue Event Listners");
+// });
+
+
+// Event Bubbling and Event Capturing ko stop karna bali balue 
+
+// green.addEventListener('click', (e) => {
+
+//     e.stopPropagation()
+//     console.log("Green Event Listners");
+// });
+
+// pink.addEventListener('click', (e) => {
+//     e.stopPropagation()
+//     console.log("Pink Event Listners");
+// });
+
+// blue.addEventListener('click', (e) => {
+//     e.stopPropagation()
+//     console.log("Blue Event Listners");
+// });
+
+// green.addEventListener('click', (e) => {
+//     console.log("Green Event Listners");
+// },{capture:true});
+
+// pink.addEventListener('click', (e) => {
+//     console.log("Pink Event Listners");
+// },{capture:true});
+
+// blue.addEventListener('click', (e) => {
+//     console.log("Blue Event Listners");
+// },{capture:true});
+
+
+// green.addEventListener('click', (e) => {
+//     console.log("Green Event Listners");
+// },{once:true});
+
+// pink.addEventListener('click', (e) => {
+//     console.log("Pink Event Listners");
+// },{once:true});
+
+// blue.addEventListener('click', (e) => {
+//     console.log("Blue Event Listners");
+// },{once:true});
--- JavaScript-First-Project/script.js
+++ JavaScript-First-Project/script.js
@@ -1,28 +1,83 @@
+const checkboxList = document.querySelectorAll(".custom-checkbox");
+const allInputFillds = document.querySelectorAll(".goal-input");
+const ErrorLable = document.querySelector(".error-lable");
+let progressLable = document.querySelector(".progress-lable");
+const progressbar = document.querySelector(".progress-bar");
+const progressValue = document.querySelector(".progress-value");
 
+const allQuotes = [
+  "Raise the bar by complating your goals",
+  "well begun is half done",
+  "just a step away, keep going!",
+  "wow! you just completed all the goals, time for chill :D",
+];
 
-const checkboxList = document.querySelectorAll('.custom-checkbox')
-const allInputFillds = document.querySelectorAll('.goal-input')
-const ErrorLable = document.querySelector('.error-lable')
-const progressbar = document.querySelector('.progress-bar')
-const progressValue = document.querySelector('.progress-value')
+const allGoals = JSON.parse(localStorage.getItem("allGoals")) || {
+    first:{
+        name: '',
+        completed: false,
+    },
+    second:{
+        name: '',
+        completed: false,
+    },
+    third:{
+        name: '',
+        completed: false,
+    }
+};
 
-checkboxList.forEach((checkbox)=>{
-    checkbox.addEventListener('click', (e)=>{
-        const allgoalsadded = [...allInputFillds].every(function(input){
-            return input.value
-        })
+let completedGoalsCount = Object.values(allGoals).filter(
+  (goal) => goal.completed
+).length;
 
-        if(allgoalsadded){
-        checkbox.parentElement.classList.toggle('completed')
-        }else{
-            progressbar.classList.add('show-error')
-            progressValue.computedStyleMap.width = "33.33%"
-        }
-    })
-})
+progressValue.style.width = `${(completedGoalsCount / 3) * 100}%`;
+progressValue.firstElementChild.innerText = `${completedGoalsCount}/3 completed`;
+progressLable.innerText = allQuotes[completedGoalsCount];
 
-allInputFillds.forEach((input)=>{
-    input.addEventListener('focus', ()=>{
-        progressbar.classList.remove('show-error')
-    })
-})
+checkboxList.forEach((checkbox) => {
+  checkbox.addEventListener("click", (e) => {
+    const allgoalsadded = [...allInputFillds].every(function (input) {
+      return input.value;
+    });
+
+    if (allgoalsadded) {
+      checkbox.parentElement.classList.toggle("completed");
+
+      const inputId = checkbox.nextElementSibling.id;
+      allGoals[inputId].completed = !allGoals[inputId].completed;
+      completedGoalsCount = Object.values(allGoals).filter(
+        (goal) => goal.completed
+      ).length;
+      progressValue.style.width = `${(completedGoalsCount / 3) * 100}%`;
+      progressValue.firstElementChild.innerText = `${completedGoalsCount}/3 completed`;
+      progressLable.innerText = allQuotes[completedGoalsCount];
+      localStorage.setItem("allGoals", JSON.stringify(allGoals));
+
+    } else {
+      progressbar.classList.add("show-error");
+    }
+  });
+});
+
+allInputFillds.forEach((input) => {
+  input.value = allGoals[input.id].name;
+
+  if (allGoals[input.id].completed) {
+    input.parentElement.classList.add("completed");
+  }
+
+  input.addEventListener("focus", () => {
+    progressbar.classList.remove("show-error");
+  });
+
+  input.addEventListener("input", (e) => {
+    if (allGoals[input.id].completed) {
+      input.value = allGoals[input.id].name;
+      return;
+    }
+
+    allGoals[input.id].name = input.value
+    localStorage.setItem("allGoals", JSON.stringify(allGoals));
+  });
+});
