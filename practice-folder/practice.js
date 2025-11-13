50bd62823a9c94fc38e4205a668e086280ec9ef2
Anilkumar9540 <anil@hitechmentor.com>
2025-11-13 13:09:28 +0530

    Added some code

--- practice-folder/practice.js
+++ practice-folder/practice.js
@@ -1983,4 +1983,32 @@
 // document.write(now.getDate()+"/"+now.getMonth()+"/"+now.getFullYear())
 
 
-        
+
+      //    Interview Quetions 
+
+// let str = "I am Anil";
+
+// let splitedString = str.split("").reverse();
+// let reversedArr = [];
+// splitedString.forEach((Element)=> {
+//         let split = Element.split("").reverse()
+//         reversedArr.push(split);
+// })
+// console.log(reversedArr)
+
+let str1 = "anil";
+let str2 = "Kumar";
+let str3 = "";
+
+function mergeString(){
+        for(let i = 0; i<str1.length+str2.length; i++){
+                if(i < str1.length){
+                        str3 = str3+str1[i];
+                }
+                if(i < str2.length){
+                        str3 = str3+str2[i]
+                }
+        }
+}
+mergeString()
+console.log(str3)
