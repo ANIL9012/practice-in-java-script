659bb436f3644ffd3dd25944f93fa09b4a570a17
Anilkumar9540 <9012kumaranil@gmail.com>
2025-12-19 11:59:20 +0530

    DataType/

---
+++ InterviewQuestions/DataType/script.js
@@ -0,0 +1,45 @@
+// let null = null;
+//  console.log(typeof null) //object // null actually primitive hai lekin JS ka Old bug hone ki bajah se "object" return karta hai
+
+// let NaN = NaN
+// console.log(typeof NaN) //"number" // NaN Not a Number hai lekin type "number" hi hota hai 
+
+// let arr = [] == []
+// console.log(arr) //false // kyuki arrays reference type hote hai value compare nahi hoti
+
+// let arr2 = [] == ![];
+// console.log(arr2); //true
+
+// let function = function(){}
+// console.log(typeof function) ; //"function" // JS mai function bhi object hota hai, lekin typeof alag value deta hai।
+
+// let undefinedNull = undefined == null
+// console.log(undefinedNull); //true
+
+// let undefinedNull2 =  undefined === null;
+// console.log(undefinedNull2) //false
+
+
+// let obj = {} + {} ; //"[object Object][object Object]"
+// lekin browser console mai NaN bhi dikha sakta hai (context par depend karta hai)
+
+
+// JavaScript me total kitne data types hote hain?
+
+// 8 Data Types
+
+// String
+
+// Number
+
+// Boolean
+
+// Undefined
+
+// Null
+
+// Symbol
+
+// BigInt
+
+// Object
