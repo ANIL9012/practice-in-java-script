a1fb111e2fa0f1e88d3a8d9e51daa127e47f53e0
Anilkumar9540 <9012kumaranil@gmail.com>
2025-12-20 09:44:42 +0530

    InterviewQuestions/Operator/

---
+++ InterviewQuestions/Operator/script.js
@@ -0,0 +1,76 @@
+// // Question:
+// // == vs ===
+// let ope = 0 == false
+// console.log(ope)// true
+// let ope2 = 0 === false
+// console.log(ope2)//false
+
+// // null vs undefined
+
+// let ope3 = null == undefined 
+// console.log(ope3)//true
+// let ope4 = null === undefined
+// console.log(ope4)// false
+// let ope5 = typeof null;
+// console.log(ope5)// "object"
+// let ope6 = 1 + "2" + 3
+// console.log(ope6)// "123"
+// let ope7 = 1 + 2 + "3"
+// console.log(ope7)// "33"
+
+// // Unary + Operator
+
+// let ope8 = +"5" + 1
+// console.log(ope8);// 6
+
+// // Boolean with Numbers
+
+// let ope9 = true + true;
+// console.log(ope9);// 2
+// let ope10 = false + true;
+// console.log(ope10)// 1
+
+// // Logical OR (||)
+
+// let ope11 = 0 || "Hello"
+// console.log(ope11);// "Hello"
+// let ope12 = "" || "JS";
+// console.log(ope12);// "JS"
+
+// // Logical AND (&&)
+
+// let ope13 = "Hi" && 0;
+// console.log(ope13);// 0
+// let ope14 = true && "JS";
+// console.log(ope14)// "JS"
+
+// // !! Operator
+
+// let ope15 = !!"JavaScript";
+// console.log(ope15);// true
+// let ope16 = !!0;
+// console.log(ope16)// false
+
+// // Post vs Pre Increment
+
+// let y = 5;
+// console.log(y++);// 5
+// console.log(++y);// 7
+
+// // Bitwise Operator
+
+// let bit1 = 5 & 1
+// console.log(bit1);// 1
+// let bit2 = 5 | 1;// 5
+
+// // delete Operator
+
+// let obj = { a: 1 };
+// delete obj.a;
+// obj.a;
+// // undefined
+
+// // Ternary Operator
+
+// let x = 0;
+// let result = x ? "Yes" : "No";// "No"
