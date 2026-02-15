103410126217ebbb568bbaf159c56b2212677306
Anil kumar <9012kumaranil@gmail.com>
2026-02-15 20:50:43 +0530

    String-Method

---
+++ String-Method/script.js
@@ -0,0 +1,78 @@
+// 1 length
+// let str = "Hello";
+// console.log(str.length); // 5
+
+// 2 toUpperCase() / toLowerCase()
+// let str = "Hello";
+// console.log(str.toUpperCase()); // HELLO
+// console.log(str.toLowerCase()); // hello
+
+// 3 trim()
+
+// (extra spaces remove karta hai)
+
+// let str = "  Hi  ";
+// console.log(str.trim()); // "Hi"
+
+// 4 slice(start, end)
+// let str = "JavaScript";
+// console.log(str.slice(0, 4)); // Java
+
+// 5 substring(start, end)
+// let str = "JavaScript";
+// console.log(str.substring(4, 10)); // Script
+
+// 6 replace()
+// let str = "I love Java";
+// console.log(str.replace("Java", "JS")); // I love JS
+
+// 7 replaceAll()
+// let str = "a a a";
+// console.log(str.replaceAll("a", "b")); // b b b
+
+// 8 includes()
+// let str = "Hello World";
+// console.log(str.includes("World")); // true
+
+// 9 indexOf()
+// let str = "Hello World";
+// console.log(str.indexOf("World")); // 6
+
+// split()
+// let str = "apple,banana,orange";
+// console.log(str.split(","));
+// // ["apple", "banana", "orange"]
+
+// 11 charAt()
+// let str = "Hello";
+// console.log(str.charAt(1)); // e
+
+// 12 startsWith() / endsWith()
+// let str = "Hello World";
+// console.log(str.startsWith("Hello")); // true
+// console.log(str.endsWith("World"));   // true
+
+// 13 concat()
+// let a = "Hello";
+// let b = "World";
+// console.log(a.concat(" ", b)); // Hello World
+
+// 14 repeat()
+// let str = "Hi ";
+// console.log(str.repeat(3)); // Hi Hi Hi
+
+// Important for interviews:
+
+// length
+
+// toUpperCase() / toLowerCase()
+
+// trim()
+
+// slice()
+
+// replace()
+
+// split()
+
+// includes()
