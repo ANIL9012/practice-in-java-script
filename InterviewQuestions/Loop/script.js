7bea3e89fe06597a7536fc7b82efa41f9c94d9b3
Anil kumar <9012kumaranil@gmail.com>
2026-02-25 18:58:02 +0530

    InterviewQuestions\Loop

--- InterviewQuestions/Loop/script.js
+++ InterviewQuestions/Loop/script.js
@@ -99,4 +99,12 @@
 
 // console.log(rev);
 
+// for (let i = 1; i <= 5; i++) {
+//   if (i == 3) break;
+//   console.log(i);
+// }
 
+// for (let i = 1; i <= 5; i++) {
+//   if (i == 3) continue;
+//   console.log(i);
+// }
