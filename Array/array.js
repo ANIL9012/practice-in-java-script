eef5809fe47c3d055b66beed9df40ba78658dcbe
Anil Kumar <9012kumaranil@gmail.com>
2026-04-30 19:43:09 +0530

    Array

--- Array/array.js
+++ Array/array.js
@@ -1,8 +1,8 @@
 // array======>
-    //            an array is an object in javascript that can store multple values at once .
-    // let a =1;
-    // let b=2;
-    // let c=3;
+//            an array is an object in javascript that can store multple values at once .
+// let a =1;
+// let b=2;
+// let c=3;
 
 // example:
 //     let array=[1,2,3,4,5,6,7,7,8]
@@ -12,158 +12,138 @@
 //     let words={"hello", "javascript","hitechmentor"} wrong
 //     let words=["hello", "javascript","hitechmentor"]
 
-
-
 //    2. create an array
 //    2. create an array
 //    2. create an array
-
 
 //         1. using array litral
 //         2. using new keyword
 
-        // syntax:1. using array litral
-        //        let arrayname=[1,2,3,4,];
+// syntax:1. using array litral
+//        let arrayname=[1,2,3,4,];
 
+//   syntax :2  using new keyword
 
-        //   syntax :2  using new keyword
-          
-        //         let array1=new Array("hello","javascript");
+//         let array1=new Array("hello","javascript");
 
-        //     console.log(array1);
-                // array can store array, function , other objects inside an array ;
+//     console.log(array1);
+// array can store array, function , other objects inside an array ;
 
-                // const newData=[
-                //               {carmodel:"1m133"},
-                //               [1,2,344,45,],
-                //               function(){
-                //                 console.log("hello .......")
-                //               }
+// const newData=[
+//               {carmodel:"1m133"},
+//               [1,2,344,45,],
+//               function(){
+//                 console.log("hello .......")
+//               }
 
-                // ];
+// ];
 
-            
+// let array1=1;
+// let array2=4;
+// let array3=["A","B","C","D"];
 
+// let finalArray=[array1,array2,array3]
+// console.log(finalArray[0])
+// console.log(finalArray[2])
+// console.log(finalArray[2][1])
 
-                // let array1=1;
-                // let array2=4;
-                // let array3=["A","B","C","D"];
+// access of array
+// access of array
+// access of array
+// access of array
 
-                // let finalArray=[array1,array2,array3]
-                // console.log(finalArray[0])
-                // console.log(finalArray[2])
-                // console.log(finalArray[2][1])
+// let array=["A","B","C","D"]
 
-                // access of array
-                // access of array
-                // access of array
-                // access of array
-
-
-                // let array=["A","B","C","D"]
-
-
-                // console.log(array[0])
-                // console.log(array[3])
-                // console.log(array[7])
-                // console.log(array)
-                // console.log(array[0])
-                // console.log(array[3])
-                // console.log(array[7])
-                // console.log(array)
-
+// console.log(array[0])
+// console.log(array[3])
+// console.log(array[7])
+// console.log(array)
+// console.log(array[0])
+// console.log(array[3])
+// console.log(array[7])
+// console.log(array)
 
 //    2.unshift ====start
 
 // array.push("push")
 //                 console.log(array)
 //                 console.log(array[4])
-                // console.log(array)
-                // console.log(array[4])
-
-
+// console.log(array)
+// console.log(array[4])
 
 // array.unshift("unshift")
 //                 console.log("unshift",array[0])
 //                 console.log(array)
-                // console.log("unshift",array[0])
-                // console.log(array)
+// console.log("unshift",array[0])
+// console.log(array)
 
-                // console.log(array[3])
-                // console.log(array[7])
-                // console.log(array)
-                // console.log(array[4])
-                // console.log(array[3])
-                // console.log(array[7])
-                // console.log(array)
-                // console.log(array[4])
+// console.log(array[3])
+// console.log(array[7])
+// console.log(array)
+// console.log(array[4])
+// console.log(array[3])
+// console.log(array[7])
+// console.log(array)
+// console.log(array[4])
 // array.unshift("E")
 //                 console.log(array)
-                // console.log(array)
-
-
-// change value
+// console.log(array)
 
 // change value
+
+// change value
 // change value
 
-                // array[3]="hitech";
-                // console.log(array)
-                // array[3]="hitech";
-                // console.log(array)
+// array[3]="hitech";
+// console.log(array)
+// array[3]="hitech";
+// console.log(array)
 
-
-                // let newarray=[1,2,3,4,5]
-                // console.log(newarray[0])
-                // console.log(newarray[9])
-                // console.log(newarray)
-                // let newarray=[1,2,3,4,5]
-                // console.log(newarray[0])
-                // console.log(newarray[9])
-                // console.log(newarray)
-
-
-
+// let newarray=[1,2,3,4,5]
+// console.log(newarray[0])
+// console.log(newarray[9])
+// console.log(newarray)
+// let newarray=[1,2,3,4,5]
+// console.log(newarray[0])
+// console.log(newarray[9])
+// console.log(newarray)
 
 // 1.pop ==last
 // 2.shift ===start
 // let newarray1=[1,2,3,4,5]
 //                 console.log("length======>",newarray.length)
 //                 console.log(newarray1[0])
-                // console.log("length======>",newarray.length)
-                // console.log(newarray1[0])
+// console.log("length======>",newarray.length)
+// console.log(newarray1[0])
 
-                // console.log(newarray1[9])
-                // console.log(newarray1)
-                // console.log(newarray1[9])
-                // console.log(newarray1)
+// console.log(newarray1[9])
+// console.log(newarray1)
+// console.log(newarray1[9])
+// console.log(newarray1)
 
-                // newarray.pop();
-                // console.log(newarray)
-                // newarray1.shift()
-                // console.log(newarray1)
-                // newarray.pop();
-                // console.log(newarray)
-                // newarray1.shift()
-                // console.log(newarray1)
-
+// newarray.pop();
+// console.log(newarray)
+// newarray1.shift()
+// console.log(newarray1)
+// newarray.pop();
+// console.log(newarray)
+// newarray1.shift()
+// console.log(newarray1)
 
 // Array lengt
 // Array length
 
 // let lengtharray=newarray1.length;
 //                 console.log("length======>",lengtharray)
-                // console.log("length======>",lengtharray)
-
-
+// console.log("length======>",lengtharray)
 
 // array methods
 // array methods
 
-// 1.concat() 
+// 1.concat()
 // 2.indexOf()
 // 3.find ()
-// 4.findindex() 
+// 4.findindex()
 // 5.foreach()
 // 6. map()
 // 7.filter()
@@ -174,10 +154,10 @@
 // 11.sort()
 // 12.slice()
 // 13.splice()
-// 1.concat() 
+// 1.concat()
 // 2.indexOf()
 // 3.find ()
-// 4.findindex() 
+// 4.findindex()
 // 5.foreach()
 // 6. map()
 // 7.filter()
@@ -189,31 +169,16 @@
 // 12.slice()
 // 13.splice()
 
-
-// concat
-// concat
-// concat
-// concat
-// concat
 // concat
 
 // let fruit="grapes";
 
-
-
-
 // let fruits=["mango","apple","orange",fruit]
 
 // let arrayfruits2=["papaya","pineapple"]
-// console.log(fruits.concat(arrayfruits2)) 
+// console.log(fruits.concat(arrayfruits2))
 
 // let tokri=fruits.concat(arrayfruits2)
-
-               
-
-
-
-
 
 // let car1=["maruti800","swift","thar"]
 // let car2=["bolero","bmw","scorpio"]
@@ -222,28 +187,17 @@
 // let CAR=car1.concat(car2,car3)
 // console.log("CAR======>",CAR)
 
-
-               
 // 2.indexOf()
-// 2.indexOf()
-// 2.indexOf()
-// serches and element of an array and return its position 
+// serches and element of an array and return its position
 // let ages=[1,2,4,56,23,21,21,4,12,24,2,4]
-
 
 // let position=ages.indexOf(4)
 // console.log(position)
 
 // 3.find ()
-// 3.find ()
-// 3.find ()
-// 3.find ()
 // return the first value of an array element that passes a condition
 
-
-
 // let AGE=[1,2,4,56,23,21,21,4,12,24,2,4]
-
 
 // function Checkage(age){
 // return age >20
@@ -257,28 +211,25 @@
 // myfunction();
 // myfunction();
 
-
 // 4.findindex()
-                // returns the first index of an array element that passes a condition
+// returns the first index of an array element that passes a condition
 
+// const numbers =[30,37,45,54,20,10,63]
+// let index=numbers.findIndex(num=>num>40)
+// console.log(index)
 
-                // const numbers =[30,37,45,54,20,10,63]
-                // let index=numbers.findIndex(num=>num>40)
-                // console.log(index)
+//  5.        foreach()
+//  5.        foreach()
+//  5.        foreach()
+//  5.        foreach()
 
-
-        //  5.        foreach()            
-        //  5.        foreach()            
-        //  5.        foreach()            
-        //  5.        foreach()  
-        
 //         const num=[1,2,3,4,5,6,7,8,9,10]
 
 //         let newnum=num.forEach(myfunction);
 
 // function myfunction(val,index,arr){
 //     sq=val*val
-//     console.log(`value=${val} and index=${index} and square is ${sq}  and array ${arr} ` )  
+//     console.log(`value=${val} and index=${index} and square is ${sq}  and array ${arr} ` )
 // }
 
 // console.log(num)
@@ -290,7 +241,7 @@
 //         console.log(array)
 //     )
 
-// addition 
+// addition
 //     const number=[20,30,34,16,37,50]
 
 //     sum=0
@@ -301,8 +252,7 @@
 // console.log(number)
 // console.log(newnumber)
 
-
-// 6. map()==its return new array , and itrate over an array 
+// 6. map()==its return new array , and itrate over an array
 
 // const Student=[20,30,34,16,37,50]
 
@@ -315,18 +265,9 @@
 
 // console.log(newstudent)
 
-
-
-
 // 7.filter()
-// 7.filter()
-// 7.filter()
-// 7.filter()
-
 
 // const voterAge=[20,30,34,16,37,56,24,12,34,32,25,12,8]
-
-
 
 // const result=voterAge.filter(checkAdult);
 
@@ -336,10 +277,8 @@
 
 // console.log(result)
 
-
 // const result=voterAge.filter((age)=>age>18);
 // console.log(result)
-
 
 // 8.includes()
 // 8.includes()
@@ -350,12 +289,8 @@
 // let resulttext=text.includes("universe");
 // console.log(resulttext)
 
-
 // Sort()
-// Sort()
-// Sort()
-// Sort()
-// sort elements aphabetically in string and number in asecending order 
+// sort elements aphabetically in string and number in asecending order
 
 // const Names=["anil","shahibe","monish","shoaib","rehan","afzal"]
 //  let newNames=Names.sort();
@@ -365,49 +300,29 @@
 //   let newSortNum=sortNum.sort();
 //   console.log(newSortNum)
 
-
-  // 12.slice()
-  // 12.slice()
-  // 12.slice()
-  // 12.slice()
-  // 12.slice(startindex(inclusive),endindex(exclusive))
+// 12.slice()
+// 12.slice(startindex(inclusive),endindex(exclusive))
 
 //  let sliceNum=[23,1,3,4,53,2,5,12,3,87,12]
 //  let newslicenum=sliceNum.slice(2,5)
 //  console.log(newslicenum)
-  
-
 
 //  let slicefruits=["mango","apple","orange","papaya","pineapple","grapes","guava"]
 // let newslicefruits=slicefruits.slice(2,6)
 // console.log(newslicefruits)
 
-
-// something wrong 
+// something wrong
 // let newslicefruits2=slicefruits.slice(-1,-3)
 // console.log(newslicefruits2)
 
-
-// splice 
-// splice 
-// splice 
-// splice 
-// splice 
-// splice 
+// splice
 // splice (startindex,counting,newelement....)
 // let splicefruits=["mango","apple","orange","papaya","pineapple","grapes","guava"]
 // let removesplice=splicefruits.splice(2,4,"coconut","dates")
 // console.log(splicefruits)
 // console.log(removesplice)
 
-
-
-
-//multidimention array 
-//multidimention array 
-//multidimention array 
-
-
+//multidimention array
 // let mularray=[
 //    0 ["java","javascript"],
 //         0       1
@@ -417,34 +332,31 @@
 //          0    1
 // ]
 
-
-
 // mularray[0][0]=java
 
 // let mularray=[["java","javascript"],["python","dbms","dotnet","mongodb",],["DSA","OS"]]
 
-
 // let mularray=[
 //     ["java","javascript"],
-        
+
 //     ["python","dbms","dotnet","mongodb",["cyber-sec","os",["c++","Ruby"]]],
-          
+
 //     ["DSA","OS",["2","4","6",["34","45","57"["23"]]]]
-        
+
 //  ]
 //  console.log(mularray[1][4][2][0])
 
 //  console.log("print 45")
-//  console.log("anil",mularray[2][2][1])//4 
-//  console.log("shoeb",mularray[2][2][3][1]) 
-//  console.log("afzal",mularray[2][2][3][1]) 
-//  console.log("monish",mularray[2][2][3][1]) 
-//  console.log("what should be print here ???",mularray[2][1][3]) 
-//  console.log("what should be print here ???",mularray[1][1][4]) 
-//  console.log("what should be print here ???",mularray[1][4]) 
-//  console.log("what should be print here ???",mularray[1][4][1]) 
-//  console.log("what should be print here ???",mularray[1][4][2]) 
-//  console.log("what should be print here ???",mularray[1][4][2][1]) 
+//  console.log("anil",mularray[2][2][1])//4
+//  console.log("shoeb",mularray[2][2][3][1])
+//  console.log("afzal",mularray[2][2][3][1])
+//  console.log("monish",mularray[2][2][3][1])
+//  console.log("what should be print here ???",mularray[2][1][3])
+//  console.log("what should be print here ???",mularray[1][1][4])
+//  console.log("what should be print here ???",mularray[1][4])
+//  console.log("what should be print here ???",mularray[1][4][1])
+//  console.log("what should be print here ???",mularray[1][4][2])
+//  console.log("what should be print here ???",mularray[1][4][2][1])
 
 //  mularray.push("1,3,46,8,8,89,8")
 // mularray[0].push("1,3,46,8,8,89,8")
@@ -455,23 +367,18 @@
 // mularray[1][4].pop()
 // console.log(mularray)
 
-
-
-// iterating over studentdata 
-// iterating over studentdata 
-// iterating over studentdata 
-// iterating over studentdata 
-// iterating over studentdata 
-// iterating over studentdata 
-
-
-
+// iterating over studentdata
+// iterating over studentdata
+// iterating over studentdata
+// iterating over studentdata
+// iterating over studentdata
+// iterating over studentdata
 
 // let studentdata1=[
 //     ['jack',23],
 //     ['sara',25]
 // ];
-// // iterating over studentdata 
+// // iterating over studentdata
 
 // studentdata1.forEach((val)=>{
 //         val.forEach((data,ind)=>{
@@ -479,7 +386,6 @@
 //         })
 
 // })
-
 
 // 2. iterating over studentdata for-----of
 
@@ -492,6 +398,3 @@
 //         console.log(j)
 //     }
 // }
-
-
-
