591b52219511cc945570b998118d27d91d044a45
Anil Kumar <9012kumaranil@gmail.com>
2026-07-25 18:04:58 +0530

    FreeCodeCampReactJsCode\01_BuildaRandomQuoteMachine

--- FreeCodeCampReactJsCode/01_BuildaRandomQuoteMachine/script.js
+++ FreeCodeCampReactJsCode/01_BuildaRandomQuoteMachine/script.js
@@ -1,26 +1,3 @@
-// !! IMPORTANT README:
-
-
-/***********
-INSTRUCTIONS:
-  - Select the project you would 
-    like to complete from the dropdown 
-    menu.
-  - Click the "RUN TESTS" button to
-    run the tests against the blank 
-    pen.
-  - Click the "TESTS" button to see 
-    the individual test cases. 
-    (should all be failing at first)
-  - Start coding! As you fulfill each
-    test case, you will see them go   
-    from red to green.
-  - As you start to build out your 
-    project, when tests are failing, 
-    you should get helpful errors 
-    along the way!
-    ************/
-
 const quotes = [
   {
     text: "Be yourself; everyone else is already taken.",
