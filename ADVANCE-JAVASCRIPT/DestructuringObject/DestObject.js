e96161a2185ef344243dee94ff664abb751dc057
Anil Kumar <9012kumaranil@gmail.com>
2026-08-20 17:24:14 +0530

    ADVANCE-JAVASCRIPT\DestructuringObject

--- ADVANCE-JAVASCRIPT/DestructuringObject/DestObject.js
+++ ADVANCE-JAVASCRIPT/DestructuringObject/DestObject.js
@@ -1,4 +1,3 @@
-
 // let obj = {
 //     name : "Anil Kuamr",
 //     age : 23,
@@ -10,13 +9,12 @@
 // console.log(age);
 // console.log(city);
 
-let obj2 = {
-    name : "Anil Kuamr",
-    age : 23,
-    city : "Rampur"
-}
+// let obj2 = {
+//     name : "Anil Kuamr",
+//     age : 23,
+//     city : "Rampur"
+// }
 
-let {name : a, age : b, city : c} = obj2;
-console.log(a);
-console.log(c);
-
+// let {name : a, age : b, city : c} = obj2;
+// console.log(a);
+// console.log(c);
