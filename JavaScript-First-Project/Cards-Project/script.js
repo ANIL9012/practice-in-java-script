646bd9649fb7bffa83ebc6112c3c4e592c5e58df
Anil Kumar <9012kumaranil@gmail.com>
2026-09-02 23:06:22 +0530

    JavaScript-First-Project\Cards-Project

--- JavaScript-First-Project/Cards-Project/script.js
+++ JavaScript-First-Project/Cards-Project/script.js
@@ -1,12 +1,12 @@
-const h1 = document.querySelector('h1')
-const container = document.querySelector('.container')
-const card = document.querySelector('.card1')
+// const h1 = document.querySelector('h1')
+// const container = document.querySelector('.container')
+// const card = document.querySelector('.card1')
 
-for(let i = 2; i <= 100; i++){
+// for(let i = 2; i <= 100; i++){
 
-   const newCard = card.cloneNode(true);
-   newCard.innerText = i;
-   container.appendChild(newCard);
-}
+//    const newCard = card.cloneNode(true);
+//    newCard.innerText = i;
+//    container.appendChild(newCard);
+// }
 
-container.classList.add("cards-display")
+// container.classList.add("cards-display")
