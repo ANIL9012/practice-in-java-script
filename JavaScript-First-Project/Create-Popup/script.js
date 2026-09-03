c9903a4454038d0ff1a16f75c05f208bb44545d5
Anil Kumar <9012kumaranil@gmail.com>
2026-09-03 18:45:02 +0530

    JavaScript-First-Project\Create-Popup

--- JavaScript-First-Project/Create-Popup/script.js
+++ JavaScript-First-Project/Create-Popup/script.js
@@ -1,17 +1,17 @@
 
-const button = document.querySelector('button')
-const closeIcon = document.querySelector('.close-icon')
-const popup = document.querySelector('.popup-container')
-const overlay = document.querySelector('.overlay')
+// const button = document.querySelector('button')
+// const closeIcon = document.querySelector('.close-icon')
+// const popup = document.querySelector('.popup-container')
+// const overlay = document.querySelector('.overlay')
 
-button.addEventListener('click', () => {
-    popup.classList.add('popup-open')
-})
+// button.addEventListener('click', () => {
+//     popup.classList.add('popup-open')
+// })
 
-closeIcon.addEventListener('click', () => {
-    popup.classList.remove('popup-open')
-})
+// closeIcon.addEventListener('click', () => {
+//     popup.classList.remove('popup-open')
+// })
 
-overlay.addEventListener('click', () => {
-    popup.classList.remove('popup-open')
-})
+// overlay.addEventListener('click', () => {
+//     popup.classList.remove('popup-open')
+// })
