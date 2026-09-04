6b0df4af02480167126949af0c0c362e22540a44
Anil Kumar <9012kumaranil@gmail.com>
2026-09-04 16:07:17 +0530

    JavaScript-First-Project\Event-Bubbling-and-Event-Capturing

--- JavaScript-First-Project/Event-Bubbling-and-Event-Capturing/script.js
+++ JavaScript-First-Project/Event-Bubbling-and-Event-Capturing/script.js
@@ -1,8 +1,8 @@
 
 
-const green = document.querySelector(".green");
-const pink = document.querySelector(".pink");
-const blue = document.querySelector(".blue")
+// const green = document.querySelector(".green");
+// const pink = document.querySelector(".pink");
+// const blue = document.querySelector(".blue")
 
 
 // green.addEventListener('click', (e) => {
