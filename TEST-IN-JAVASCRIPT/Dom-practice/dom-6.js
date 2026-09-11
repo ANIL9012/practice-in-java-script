c5355c733908836887a74659d3dd9a7f5297650d
Anil Kumar <9012kumaranil@gmail.com>
2026-09-11 18:34:59 +0530

    TEST-IN-JAVASCRIPT\Dom-practice

--- TEST-IN-JAVASCRIPT/Dom-practice/dom-6.js
+++ TEST-IN-JAVASCRIPT/Dom-practice/dom-6.js
@@ -1,18 +1,18 @@
-        //   scrollBy & scrollTo
+//         //   scrollBy & scrollTo
                 
-// function scrollWindow(){
-//     // window.scrollBy(50,0)
-//     window.scrollTo(50,0)
+// // function scrollWindow(){
+// //     // window.scrollBy(50,0)
+// //     window.scrollTo(50,0)
+// // }
+
+
+
+//             //    Location Object method 
+
+
+// function newLocation(){
+//     // location.href="https://www.google.com";
+//     // location.reload();
+//     // location.assign("https://www.google.com");
+//     location.replace("https://www.google.com");
 // }
-
-
-
-            //    Location Object method 
-
-
-function newLocation(){
-    // location.href="https://www.google.com";
-    // location.reload();
-    // location.assign("https://www.google.com");
-    location.replace("https://www.google.com");
-}
